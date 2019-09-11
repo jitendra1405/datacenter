1cba4d140e9f8bd413e0e6a5ab3216d8fffd2332
jitendra1405 <53037241+jitendra1405@users.noreply.github.com>
2019-09-11 15:23:35 +0530

    Update web.js

--- web.js
+++ web.js
@@ -4,10 +4,10 @@ var app = express();
 app.use(express.logger());
 
 var db_config = {
-    host: 'us-cdbr-east-04.cleardb.com',
-    user: 'b6d6c6e8740d20',
-    password: 'b3f75ada',
-    database: 'heroku_1daa39da0375291'
+    host     : 'sql12.freemysqlhosting.net',
+    database : 'sql12304794',
+    user     : 'sql12304794',
+    password : 'PLSEEGHnWv'
 };
 
 var connection;
@@ -37,7 +37,7 @@ function handleDisconnect() {
 handleDisconnect();
 
 app.get('/', function(request, response) {
-    connection.query('SELECT * from t_users', function(err, rows, fields) {
+    connection.query('SELECT * from user', function(err, rows, fields) {
         if (err) {
             console.log('error: ', err);
             throw err;
